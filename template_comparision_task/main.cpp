793c4024340866d8ce62bd1ecc547490181fc9b9
Sladkoeshechka <dima277238@gmail.com>
2023-03-16 23:19:09 +0500

    refactored tests of first and second tasks

--- template_comparision_task/main.cpp
+++ template_comparision_task/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include "first_task.h"
+#include "second_task.h"
 
-int main()
+void first_task_test()
 {
     NewA a1(1, 2);
     NewA a2(2, 3);
-    std::cout << (a1 >= a2) << (a1 <= a2) << (a1 == a2) << (a1 != a2) << std::endl;;
+    std::cout << (a1 >= a2) << (a1 <= a2) << (a1 == a2) << (a1 != a2) << std::endl;
     std::cout << (a1 >= 4) << (a1 <= 4) << (a1 == 4) << (a1 != 4) << std::endl;
-    std::cout << (a1 >= 4.0) << (a1 <= 4.0) << (a1 == 4.0) << (a1 != 4.0) << std::endl;;
+    std::cout << (a1 >= 4.0) << (a1 <= 4.0) << (a1 == 4.0) << (a1 != 4.0) << std::endl;
+}
+
+void second_task_test()
+{
+    A fisrt(3, 5);
+    A second(1, 2);
+}
+
+int main()
+{
+    first_task_test();
+    second_task_test();
 }
